8026a4aa14edf267104c3863cad2618286cbf9cd
Timon Christl <timonchristl@gmail.com>
2019-02-27 20:58:26 +0100

    Migrated to .NET Standard 2.0

--- src/TC.Injector/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TC.Injector")]
-[assembly: AssemblyDescription("Timon Christl's dependency injector")]
-[assembly: ComVisible(false)]
-[assembly: Guid("32d2a9d7-b8a7-4d60-b0a8-8f9308b9b340")]
